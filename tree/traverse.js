779f7b50d6229077cffe64514e10ed537d8dbb9a
samiulalimsaad <saadnbiuraj@gmail.com>
2022-02-23 12:42:13 +0600

    added first tree

---
+++ tree/traverse.js
@@ -0,0 +1,34 @@
+class Node {
+    constructor(value) {
+        this.node = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+function dfs(root) {
+    const stack = [root];
+
+    while (stack.length) {
+        const current = stack.pop();
+        console.log(current.node);
+        if (current.right) stack.push(current.right);
+        if (current.left) stack.push(current.left);
+    }
+}
+
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+const e = new Node("e");
+const f = new Node("f");
+
+a.left = b;
+a.right = c;
+b.left = d;
+b.right = e;
+c.right = f;
+
+// console.log(a);
+dfs(a);
